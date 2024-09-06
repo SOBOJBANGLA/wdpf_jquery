c07c72b1706c5f726c725182f2d907861b89672f
SOBOJBANGLA <164489940+SOBOJBANGLA@users.noreply.github.com>
2024-09-06 22:42:25 +0600

    6ss

--- 05sep2024/get_data.php
+++ 05sep2024/get_data.php
@@ -1,26 +1,9 @@
-<?php
-
-$db= new mysqli("localhost:3310","root","","w60");
+<?php 
 
 //echo $_GET['id'];
 
-//echo "Hello world";
+echo "Hello world";
 
-$id =$_GET['id'];
-
-/*$cities =array (
-    1 =>"Dhaka is famous for mosque",
-    2 =>"Cumilla is famous for Roshmalai",
-    3 =>"Rajshahi is famous for mango"
-);
-
-echo $cities[$id];*/
-
-$sql = $db->query("SELECT * FROM city WHERE id = '$id'");
-
-$row = $sql->fetch_assoc();
-
-echo $row['name']. ": " . $row['detais'];
 
 
 ?>
